3ed39b870c77c513b47be7c309358d7f6107d5cc
math4mad <smartfamilycn@qq.com>
2023-10-12 10:21:41 +0800

    add note
    add  iris-logistics-reg.qmd

--- category/utils.jl
+++ category/utils.jl
@@ -1,4 +1,4 @@
-using  CSV,DataFrames,FileIO,GLMakie
+using  CSV,DataFrames,FileIO,GLMakie,MLJ
 
 function load_csv(str::AbstractString)::AbstractDataFrame
     df = str |> d -> CSV.File("../dataset/$str.csv") |> DataFrame |> dropmissing
@@ -18,5 +18,18 @@ function boundary_data(df::AbstractDataFrame;n=200)
 end
 
 
+function load_german_creditcard()
+    to_ScienceType(d)=coerce(d,autotype(d, (:few_to_finite, :discrete_to_continuous)))
+    df=CSV.File("../dataset/german_creditcard.csv") |> DataFrame|>to_ScienceType
+    y, X=  unpack(df, ==(:Creditability));
+    cat=levels(y)
+    X=X[:,1:end-1]  # 去除最后一列变量
+    (Xtrain, Xtest), (ytrain, ytest) = partition((X, y), 0.8, rng=123, multi=true);
+    return Xtrain, Xtest, ytrain, ytest,cat
+end
+
 
 marker_style=(marker=:circle,markersize=10,color=(:lightgreen,0.1),strokewidth=2,strokecolor=:black)
+
+
+load_german_creditcard()
